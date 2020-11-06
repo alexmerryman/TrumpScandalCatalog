eb62450695cebafc359e4e32d9b36a866eda28ff
alexmerryman <merryman.alexander@gmail.com>
2020-11-06 00:29:44 -0700

    - working on bigrams hashmap.

--- nlp_processing.py
+++ nlp_processing.py
@@ -16,6 +16,7 @@ catalog_df_sample = catalog_df.sample(n=100)
 def tokenize_POS_tag(df):
     df = df[~df['entry_text_split'].isnull()]  # TODO: Do this in preprocess?
     df['entry_no_stopwords'] = df['entry_text_split'].apply(lambda x: ' '.join([word for word in str(x).split() if word not in (stop)]))
+    # TODO: Remove punctuation?
     df['tokenized_text'] = df['entry_no_stopwords'].apply(word_tokenize)
     df['bigrams'] = df['tokenized_text'].apply(lambda row: list(nltk.ngrams(row, 2)))
     df['POS_tagged_text'] = pos_tag_sents(df['entry_text_split'].apply(word_tokenize))
@@ -26,11 +27,35 @@ def tokenize_POS_tag(df):
 tokenized_tagged_df = tokenize_POS_tag(catalog_df_sample)
 
 
+def common_NNP(df):
+    # TODO
+    # consider if 2 NNP in a row
+    # run POS tagging on bigrams? trigrams?
+    pass
+
+
+def full_flat_corpus(df):
+    return [item for sublist in df['tokenized_text'].tolist() for item in sublist]
+
+
+def bag_of_words(df):
+    # TODO - bag of words/hashmap of entire corpus
+    pass
+
+
 def bigrams_hashmap(df):
     flat_list = [item for sublist in df['bigrams'].tolist() for item in sublist]
-    bigrams_hashmap_dict = {f'{k}': flat_list.count(k) for k in flat_list}
-    print(bigrams_hashmap_dict)
-    bigrams_hashmap_df = pd.DataFrame.from_dict(data=bigrams_hashmap_dict, orient='index', columns=['bigram', 'corpus_count'])
+
+    # bigrams_hashmap_dict = {}
+    # for k in flat_list:
+    #     bigrams_hashmap_dict[str(k)] = bigrams_hashmap_dict.get(k, 0) + 1
+    # print(bigrams_hashmap_dict)
+    # bigrams_hashmap_df = pd.DataFrame.from_dict(data=bigrams_hashmap_dict, orient='index', columns=['bigram', 'corpus_count'])
+    # print(bigrams_hashmap_df)
+
+    bigrams_hashmap_list = [(f'{k}', flat_list.count(k)) for k in flat_list]
+    bigrams_hashmap_df = pd.DataFrame.from_records(data=bigrams_hashmap_list, columns=['bigram', 'corpus_count'])
+    bigrams_hashmap_df = bigrams_hashmap_df.sort_values(by='corpus_count', ascending=False)
 
     return bigrams_hashmap_df
 
@@ -42,6 +67,7 @@ def POS_hashmap(df):
 
 
 bigrams_hashmap_df = bigrams_hashmap(tokenized_tagged_df)
+bigrams_hashmap_df.to_csv("bigrams_hashmap.csv")
 print(bigrams_hashmap_df)
 
 tokenized_tagged_df.to_csv("nlp_df.csv", index=False)
